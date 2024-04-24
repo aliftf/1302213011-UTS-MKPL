895b5b03cf836371e6103ef190fb40c5f591f0b4
Alif T <alift929@gmail.com>
2024-04-24 08:40:43 +0700

    Menghapus dan memindahkan komentar tidak penting
    Menghapus dan memindahkan komentar yang tidak penting pada method di class employee

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -58,12 +58,10 @@ public class Employee {
 		return this.family;
 	}
 	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {	
+	public void setMonthlySalary(int grade) {
+		
+		/* Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya. */
+		
 		if (grade == 1) {
 			monthlySalary = 3000000;
 		}else if (grade == 2) {
@@ -87,7 +85,8 @@ public class Employee {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+		/* Menghitung berapa lama pegawai bekerja dalam setahun ini. */
+		
 		LocalDate date = LocalDate.now();
 		
 		int monthWorkingInYear = (date.getYear() == dateJoined.getYear()) ? date.getMonthValue() - dateJoined.getMonthValue() : 12;
